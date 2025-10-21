6c640e1719acd1484fa7fb993238667eab872e4f
Anderson Riera <andersonriera10@gmail.com>
2025-10-21 17:57:28 -0500

    fix: bind Flask to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -251,4 +251,5 @@ def eliminar(producto_id):
 # 🔹 Iniciar servidor
 # ------------------------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
